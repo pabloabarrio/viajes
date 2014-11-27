b4229384dd41b4adbd944666ecc6808addbb6b2b
pabloabarrio <pabloa.barriodiez@gmail.com>
2014-11-27 13:30:44 +0100

    little estilado

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,7 @@ class ApplicationController < ActionController::Base
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) << :name
+      devise_parameter_sanitizer.for(:sign_up) << :team
     end
 
 end
